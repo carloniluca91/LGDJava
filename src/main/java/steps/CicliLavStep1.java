b0e1b7ec264cc57e0d35c19ffb6712d9201bf58f
carlox <carloxluca91@hotmail.it>
2019-06-24 17:44:13 +0200

    added CiclilavStep1.java

--- src/main/java/steps/CicliLavStep1.java
+++ src/main/java/steps/CicliLavStep1.java
@@ -7,7 +7,11 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.functions;
 import org.apache.spark.sql.types.*;
+import scala.collection.JavaConverters;
+import scala.collection.Seq;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class CicliLavStep1 extends AbstractStep{
@@ -32,9 +36,13 @@ public class CicliLavStep1 extends AbstractStep{
         String dataA = options.getOption("da").getValue();
         logger.info("dataDa: " + dataDa + ", dataA: " + dataA);
 
-        // 22
+        String csvFormat = getProperty("csv_format");  // retrieve csv_format from config.properties
+        logger.info("csv format: " + csvFormat);
+
         String tlbcdefPath = getProperty("TLBCDEF_PATH");
         logger.info("tlbcdefPath: " + tlbcdefPath);
+
+        // 22
         StructField[] tlbcidefColumns = new StructField[]{
 
                 new StructField("cd_isti", DataTypes.StringType, true, Metadata.empty()),
@@ -50,8 +58,6 @@ public class CicliLavStep1 extends AbstractStep{
                 new StructField("indi_impr_priv", DataTypes.StringType, true, Metadata.empty())};
 
         StructType tlbcidefSchema = new StructType(tlbcidefColumns);
-        String csvFormat = getProperty("CSV_FORMAT");  // retrieve csv_format from config.properties
-        logger.info("csv format: " + csvFormat);
         Dataset<Row> tlbcidef = sparkSession.read().format(csvFormat).option("delimiter", ",")
                 .schema(tlbcidefSchema).csv(tlbcdefPath);
 
@@ -100,11 +106,9 @@ public class CicliLavStep1 extends AbstractStep{
                         functions.min("datainizioristrutt").as("datainizioristrutt"),
                         functions.min("datainizioinc").as("datainizioinc"),
                         functions.min("datainiziosoff").as("datainiziosoff"));
-
         // 71
 
         // 78
-
         String tlbcraccPath = getProperty("TLBCRACC_PATH");
         logger.info("tlbcraccPath: " + tlbcdefPath);
         StructField[] tlbcraccColumns = new StructField[]{
@@ -127,7 +131,55 @@ public class CicliLavStep1 extends AbstractStep{
         logger.info("tlbraccFilterCol: " + tlbraccFilterCol.toString());
 
         Dataset<Row> tlbcracc = tlbcraccLoad.filter(tlbraccFilterCol);
-
         // 90
+
+        // 97
+        Dataset<Row> cicliRacc1 = tlbcidefMax.join(tlbcracc, tlbcidefMax.col("cd_isti").equalTo(tlbcracc.col("cd_isti"))
+                .and(tlbcidefMax.col("ndg_principale").equalTo(tlbcracc.col("ndg"))), "left").select(
+                        tlbcidefMax.col("cd_isti"), tlbcidefMax.col("ndg_principale"), tlbcidefMax.col("cd_isti"),
+                        tlbcidefMax.col("dt_inizio_ciclo"), tlbcidefMax.col("dt_fine_ciclo"), tlbcidefMax.col("datainiziopd"),
+                        tlbcidefMax.col("datainizioristrutt"), tlbcidefMax.col("datainizioinc"), tlbcidefMax.col("datainiziosoff"),
+                        tlbcracc.col("cod_raccordo"), tlbcracc.col("data_rif"));
+        // 110
+
+        // 119
+        List<String> joinCols = Arrays.asList("cod_raccordo", "data_rif");
+        logger.info("join columns: " + joinCols.toString());
+        logger.info("convertion to scala Seq");
+        Seq<String> joinColsSeq = JavaConverters.asScalaIteratorConverter(joinCols.iterator()).asScala().toSeq();  // conversion to scala Seq
+
+        Column cdIstiCedCol = functions.when(tlbcracc.col("cd_isti").isNotNull(), tlbcracc.col("cd_isti"))
+                .otherwise(cicliRacc1.col("cd_isti")).as("cd_isti_ced");
+        logger.info("cdIstiCedCol: " + cdIstiCedCol.toString());
+
+        Column ndgCedCol = functions.when(tlbcracc.col("ndg").isNotNull(), tlbcracc.col("ndg"))
+                .otherwise(cicliRacc1.col("ndg_principale")).as("ndg_ced");
+        logger.info("ndgCedCol: " + ndgCedCol.toString());
+
+        Dataset<Row> ciclilavStep1 = cicliRacc1.join(tlbcracc, joinColsSeq, "left").select(
+                cicliRacc1.col("cd_isti"), cicliRacc1.col("ndg_principale"), cicliRacc1.col("dt_inizio_ciclo"),
+                cicliRacc1.col("dt_fine_ciclo"), cicliRacc1.col("datainiziopd"), cicliRacc1.col("datainizioristrutt"),
+                cicliRacc1.col("datainizioinc"), cicliRacc1.col("datainiziosoff"), functions.lit(0).as("progr"),
+                cdIstiCedCol, ndgCedCol).distinct();
+        // 149
+
+        // 155
+        Column dtRifCraccCol = functions.when(tlbcracc.col("data_rif").isNotNull(), tlbcracc.col("data_rif"))
+                .otherwise(cicliRacc1.col("dt_inizio_ciclo")).as("dt_rif_cracc");
+        logger.info("dtRifCraccCol: " + dtRifCraccCol.toString());
+
+        Dataset<Row> ciclilavStep1Filecracc = cicliRacc1.join(tlbcracc, joinColsSeq, "left").select(
+                cicliRacc1.col("cd_isti"), cicliRacc1.col("ndg_principale"), cicliRacc1.col("dt_inizio_ciclo"),
+                cicliRacc1.col("dt_fine_ciclo"), cdIstiCedCol, ndgCedCol, dtRifCraccCol);
+        // 176
+
+        String ciclilavStep1Outdir = getProperty("CICLILAV_STEP1_OUTDIR");
+        logger.info("ciclilavStep1Outdir: " + ciclilavStep1Outdir);
+
+        String ciclilavStep1Outcracc = getProperty("CICLILAV_STEP1_OUTCRACC");
+        logger.info("ciclilavStep1Outcracc: " + ciclilavStep1Outcracc);
+
+        ciclilavStep1.write().format(csvFormat).option("delimiter", ",").csv(ciclilavStep1Outdir);
+        ciclilavStep1Filecracc.write().format(csvFormat).option("delimiter", ",").csv(ciclilavStep1Outcracc);
     }
 }
